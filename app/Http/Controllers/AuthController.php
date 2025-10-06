f3e0b8af8169b23f72e76ea74eb09c2e2d420fe9
imadelcass <imadelcass@gmail.com>
2025-10-06 13:44:13 +0100

    feat: Add 'me' method to AuthController for retrieving authenticated user information

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -32,12 +32,17 @@ class AuthController extends Controller
     public function register(RegisterRequest $request)
     {
         DB::beginTransaction();
-        
+
         $user = User::create($request->validated());
         $token = $user->createToken('auth')->plainTextToken;
 
         DB::commit();
 
         return response()->json(['token' => $token]);
+    }
+
+    public function me()
+    {
+        return response()->json(auth()->user());
     }
 }
