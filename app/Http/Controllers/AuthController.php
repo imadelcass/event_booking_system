b8f026da0284ee82908509de6de852a8b7f4f137
imadelcass <imadelcass@gmail.com>
2025-10-06 13:29:52 +0100

    feat: Implement authentication system with login, registration, and logout functionality

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\DB;
+
+class AuthController extends Controller
+{
+    public function login(LoginRequest $request)
+    {
+
+        if (!auth()->attempt($request->validated())) {
+            return response()->json(['message' => __('auth.failed')], Response::HTTP_UNAUTHORIZED);
+        }
+
+        $token = auth()->user()->createToken('auth')->plainTextToken;
+
+        return response()->json(['token' => $token]);
+    }
+
+    public function logout()
+    {
+        auth()->user()->tokens()->delete();
+
+        return response()->noContent();
+    }
+
+    public function register(RegisterRequest $request)
+    {
+        DB::beginTransaction();
+        
+        $user = User::create($request->validated());
+        $token = $user->createToken('auth')->plainTextToken;
+
+        DB::commit();
+
+        return response()->json(['token' => $token]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,14 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Public Routes
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    // Auth
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+});
